71fb3a65532fd0c45f1a8b0ae6e6342765cfdeaf
Achraf <achrafatauil@hotmail.com>
2019-10-24 09:50:29 +0200

    Added index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-hubhju
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <form action="" method="get">
+    <input type="text" name = "searchInput" placeholder="Search.." value = "">
+    <button type="submit">Submit</button>
+    </form>
+    <?php include('searchBar.php') ?>
+
+        
+</body>
+</html>
+
+<!-- {   "page":1, -->
