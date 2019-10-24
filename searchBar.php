b8b4c10d3dee4069bcc4f38bbd55ddac7c57cbea
Achraf <achrafatauil@hotmail.com>
2019-10-24 09:49:28 +0200

    Added Searchbar API

---
+++ searchBar.php
@@ -0,0 +1,39 @@
+<?php
+$API_key = "4af2589deef3c4d1a028374023d93f3e";
+
+    // Search bar get data
+    $searchInput =  $_GET["searchInput"];
+    $queryString = '&query='.$searchInput;
+
+    //Json CALL using cURL
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.themoviedb.org/3/search/movie?api_key=$API_key&language=en-US$queryString&page=1&include_adult=false",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_POSTFIELDS => "{}",
+    ));
+
+    $response = json_decode(curl_exec($curl));
+    $err = curl_error($curl);
+
+
+
+    curl_close($curl);
+
+    if ($err) {
+    echo "cURL Error #:" . $err;
+    } else {
+            echo '<pre>';
+            print_r($response);
+            echo '</pre>';
+        
+            
+    }
+?>
