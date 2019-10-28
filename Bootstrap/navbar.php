1983938ae1bb820d745557cec445cb461dc54fae
ShaoYuan <shaoyuan.weng@gmail.com>
2019-10-28 10:22:17 +0100

    navbar commented

--- Bootstrap/navbar.php
+++ Bootstrap/navbar.php
@@ -3,12 +3,15 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse navbars justify-content-between" id="collapse_target1">
+        <!-- logo -->
         <a class="navbar-brand text-white" href="#">FishEyes</a>
         <div class="d-flex">
+            <!-- searchBar -->
             <form method="post" id="searchBar" class="searchBar">
                 <input type="text" class="form-control" name="searchInput">
             </form>
             <button class="btn text-white shadow-none"><i class="fas fa-search text-white" id="search"></i></button>
+            <!-- login button -->
             <button class="btn text-white shadow-none" data-toggle="modal" data-target="#Modal">Log In</button>
         </div>
     </div>
@@ -16,22 +19,23 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="collapse navbar-collapse navbars justify-content-between" id="collapse_target2">
         <div class="d-flex">
+            <!-- category -->
             <form method="post"><button type="submit" name="genre" value="Comédie" class="btn text-white shadow-none pl-0">Comédie</button></form>
             <form method="post"><button type="submit" name="genre" value="Horreur" class="btn text-white shadow-none">Horreur</button></form>
             <form method="post"><button type="submit" name="genre" value="Thriller" class="btn text-white shadow-none">Thriller</button></form>
             <form method="post"><button type="submit" name="genre" value="Guerre" class="btn text-white shadow-none">Guerre</button></form>
             <form method="post"><button type="submit" name="genre" value="SF" class="btn text-white shadow-none">SF</button></form>
-            <!--<form method="post"><button type="submit" name="genre" value="Documentaire" class="btn text-white shadow-none">Documentaire</button></form>-->
         </div>
         <div class="d-flex">
             <button class="btn text-white shadow-none">Settings</button>
+            <!-- logout button -->
             <form action="login.php" method="post"><button class="btn text-white shadow-none" type="submit" name="logout">Logout</button></form>
         </div>
     </div>
 </nav>
 
     
-<!-- Modal -->
+<!-- login Modal -->
 
 <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
@@ -39,6 +43,7 @@
             <div class="modal-body container">
                 <div class="row">
                     <div class="col-6">
+                        <!-- register -->
                         <h3 class="text-center mb-4">Register</h3>
                         <form method="post" action="create.php" class="d-flex flex-column ml-3" oninput='password2.setCustomValidity(password2.value != password.value ? "Passwords do not match." : "")'>
                             <div class="form-group">
@@ -61,6 +66,7 @@
                             &nbsp;
                         </form>
                     </div>
+                    <!-- login -->
                     <div class="col-6 d-flex flex-column align-items-center justify-content-center">
                         <h3 class="text-center mb-4">Login</h3>
                         <form method="post" action="login.php" class="d-flex flex-column">
