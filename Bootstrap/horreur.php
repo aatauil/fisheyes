a282b5f8e057dd2dd671a8620033bde6b736272f
Achraf <achrafatauil@hotmail.com>
2019-10-29 13:40:44 +0100

    fix Horreur API

--- Bootstrap/horreur.php
+++ Bootstrap/horreur.php
@@ -13,18 +13,18 @@ $API_key = "4af2589deef3c4d1a028374023d93f3e";
     $curl = curl_init();
 
 curl_setopt_array($curl, array(
-CURLOPT_URL => "https://api.themoviedb.org/3/$movieDetails?api_key=$API_key&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=$genreNumber,
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_ENCODING => "",
-CURLOPT_MAXREDIRS => 10,
-CURLOPT_TIMEOUT => 100,
-CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-CURLOPT_CUSTOMREQUEST => "GET",
-CURLOPT_POSTFIELDS => "{}",
-));
+    CURLOPT_URL => 'https://api.themoviedb.org/3/$movieDetails?api_key=$API_key&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=$genreNumber',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 100,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_POSTFIELDS => "{}",
+    ));
 
 $response = json_decode(curl_exec($curl),true); 
-$movieInfo = $response['results'];
+$movieInfo = $response["results"];
 $err = curl_error($curl);
 
 curl_close($curl);
